7e876f9e7ad5a4b84f1480c1661ebdfe6b7315df
Nikita Ageev <naqfaq1403@mail.ru>
2022-12-22 01:06:08 +0500

    finish

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,20 @@ const all = document.querySelector('#all')
 const result = document.querySelector('#result')
 const btn = document.querySelector('#btn')
 
-let val = (kf * (win / all) - 1) * 100 > 1
-let valResult
 
-if (val) {
-   valResult = 'Да'
-} else {
-   valResult = 'Нет'
-}
 
 btn.addEventListener('click', () => {
+   let val = (+kf.value * (+win.value / +all.value) - 1) * 100 > 1
+   let valResult
+
+   if (val) {
+      valResult = 'Да'
+   } else {
+      valResult = 'Нет'
+   }
    result.innerHTML = `
    <p>Вероятность букмекера: ${Math.floor(1 / kf.value * 100)}%</p>
    <p>Вероятность игрока ${Math.floor(win.value / all.value * 100)}%</p>
    <p>Коэффициент игрока: ${(1 / (win.value / all.value * 100) * 100).toFixed(2)}</p>
-   <p>Валуйность: ${valResult}</p>
-   `
+   <p>Валуйность: ${valResult}</p>`
 })
